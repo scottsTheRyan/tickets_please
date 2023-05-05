526b1258600ca60c8af35c812ae187aade2927b5
thedarkness <sthomas25854@protonmail.com>
2023-05-05 15:42:09 -0700

    add JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,21 +1,21 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters')
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -29,15 +29,28 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
 );
 
 export { router as signupRouter };
-
